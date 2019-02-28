b047ae86622395649767d89ec9f1a104d3aaac7d
Matthew Deville @madev <matthew.deville@1000mercis.com>
2019-02-28 14:34:56 +0100

    Fix chat height

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,36 +1,36 @@
 <template>
-  <v-container fill-height>
-    <v-layout fill-height id="chat">
-      <v-flex md12>
-        <v-card class="elevation-12" color="primary lighten-4" fill-height>
-          <v-toolbar dark color="primary darken-1">
-            <v-toolbar-title>Chat</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-list ref="chat" id="logs">
-              <template v-for="(item, index) in logs">
-                <v-subheader v-if="item" :key="index">{{ item }}</v-subheader>
-              </template>
-            </v-list>
-          </v-card-text>
-          <v-card-actions>
-              <v-form @submit.prevent="submit">
-                <v-layout justify-space-between row wrap>
-                  <v-flex>
-                    <v-text-field v-model="msg" label="Message" single-line solo-inverted></v-text-field>
-                  </v-flex>
-                  <v-flex>
-                    <v-btn fab dark small color="primary" type="submit">
-                      <v-icon dark>send</v-icon>
-                    </v-btn>
-                  </v-flex>
-                </v-layout>
-              </v-form>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout fill-height id="chat">
+    <v-flex md12>
+      <v-card class="elevation-12 fill-height" color="primary lighten-4">
+        <v-toolbar dark color="primary darken-1">
+          <v-toolbar-title>Chat</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-list ref="chat" id="logs">
+            <template v-for="(item, index) in logs">
+              <v-subheader v-if="item" :key="index">{{ item }}</v-subheader>
+            </template>
+          </v-list>
+        </v-card-text>
+        <v-card-actions>
+            <v-flex>
+            <v-form @submit.prevent="submit">
+              <v-layout justify-space-between row wrap>
+                <v-flex>
+                  <v-text-field v-model="msg" label="Message" single-line solo-inverted></v-text-field>
+                </v-flex>
+                <v-flex>
+                  <v-btn fab dark small color="primary" type="submit">
+                    <v-icon dark>send</v-icon>
+                  </v-btn>
+                </v-flex>
+              </v-layout>
+            </v-form>
+            </v-flex>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
